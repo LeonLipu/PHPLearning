5b1c4eed71e0fac30b065b56aa7ea6533ced94a0
git <lipusipupara@gmail.com>
2015-07-20 15:35:16 +0530

    class and echo

---
+++ day1/class.php
@@ -0,0 +1,22 @@
+<?php
+
+class abc {
+
+	public function hello() {
+
+		echo "i am function 1111111111111 ";
+
+	}
+
+	function fun2() {
+		echo "i am function 222222222";
+	}
+}
+
+$ob = new abc();
+
+$ob->hello();
+
+$ob->fun2();
+
+?>
