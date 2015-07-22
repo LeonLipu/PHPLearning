f347164abb43d21357079263fba84a8b4319b1cf
git <lipusipupara@gmail.com>
2015-07-22 23:55:58 +0530

    added inheritance methods

---
+++ day2.php
@@ -0,0 +1,27 @@
+<?php
+
+class bapa {
+
+	public $var = "i am variable ";
+
+	public function getvariable() {
+
+		echo $this->var;
+	}
+}
+
+class pua extends bapa {
+
+	public $car = "maruti ";
+
+	public function getcar() {
+
+		echo $this->car;
+	}
+}
+$ob = new pua();
+$ob->getcar(); //accessing child methods
+
+$ob->getvariable(); //accessing parent methods
+
+?>
