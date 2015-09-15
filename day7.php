410b123953ff50687ea40c517908e23160f4991b
git <lipusipupara@gmail.com>
2015-09-15 06:08:24 +0530

    loop and array

---
+++ day7.php
@@ -0,0 +1,17 @@
+<?php
+
+
+session_start();
+
+if (isset($_SESSION['counter'])) {
+
+echo "something ";
+}
+else {
+  $_SESSION['counter']=1;
+}
+
+$msg = "You have visited this page ". $SESSION['counter'];
+
+echo $msg;
+?>
---
+++ loop.php
@@ -0,0 +1,16 @@
+<?php
+$arr=array("modatty","tuesday","wedesday","something and it might be best possible to this nothing");
+$assarr=array("key1"=>"value1","key2"=>"value2");
+
+
+foreach($assarr as $key=>$value){
+
+echo "$key";
+
+}
+
+foreach($arr as $element){
+  echo "$element<br>";
+}
+
+ ?>
