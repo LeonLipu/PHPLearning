6cf239652bd403581215484b53be5097b09ec265
git <lipusipupara@gmail.com>
2015-10-14 15:11:07 +0530

    pass by referece and york

---
+++ dynamiccall.php
@@ -0,0 +1,23 @@
+<?php
+
+$no =10;
+//pass by reference
+function change (&$temp){
+
+  $temp=40;
+
+}
+change($no);
+
+echo "$no";
+//dynamic function call 
+$fun = "change";
+
+$no1=30;
+$fun($no1);
+
+echo $no1;
+
+
+
+ ?>
